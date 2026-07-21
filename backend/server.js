704e3502864debfab54146c2183fc9029d315f80
balajisuryawanshi70 <surywanshib@gmail.com>
2026-07-21 18:19:51 +0530

    frontend url chnged

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(async (req, res, next) => {
 
 // Set CORS for frontend URL / allow single-node deploy
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL].filter(Boolean),
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', process.env.FRONTEND_URL].filter(Boolean),
   credentials: true
 }));
 
